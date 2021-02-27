ae05f3143cd5cb74961880eb92cea6577cb39363
Mhenty <72777530+Mhenty@users.noreply.github.com>
2021-02-27 17:49:29 +1100

    Changes so it corretly pulls branches

--- mur_init/mur_init.sh
+++ mur_init/mur_init.sh
@@ -1,14 +1,69 @@
-git clone --branch 0e62848 git@github.com:catkin/catkin_simple.git
-git clone --branch 1b8eab2 git@github.com:ethz-asl/glog_catkin.git
-git clone --branch 6f48c85 https://github.com/tu-darmstadt-ros-pkg/hector_gazebo
-git clone --branch 2e0d245 git@github.com:MURDriverless/lidar_dev.git
-git clone --branch 0342d90 git@github.com:MURDriverless/linefit_ground_segmentation.git
-git clone --branch 7faf0f9 git@github.com:MURDriverless/mur_common.git
-git clone --branch d57bbe4 git@github.com:MURDriverless/mursim.git
-git clone --branch 121d312 git@github.com:MURDriverless/mursim_actuator.git
-git clone --branch d9da07e https://github.com/MURDriverless/ouster_example
-git clone --branch 5cf5830 git@github.com:MURDriverless/path_follower_ros.git
-git clone --branch 60746ed git@github.com:MURDriverless/planner_exploratory.git
-git clone --branch 53e2ce8 git@github.com:MURDriverless/slam.git
-git clone --branch cb9a8da git@github.com:MURDriverless/localisation.git
-git clone --branch 1bab9c8 git@github.com:MURDriverless/vision_opencv.git
+git clone git@github.com:catkin/catkin_simple.git
+cd catkin_simple
+git checkout 0e62848
+cd ..
+
+git clone git@github.com:ethz-asl/glog_catkin.git
+cd glog_catkin
+git checkout 1b8eab2
+cd ..
+
+git clone https://github.com/tu-darmstadt-ros-pkg/hector_gazebo
+cd hector_gazebo
+git checkout 6f48c85
+cd ..
+
+git clone git@github.com:MURDriverless/lidar_dev.git
+cd lidar_dev
+git checkout 2e0d245
+cd ..
+
+git clone git@github.com:MURDriverless/linefit_ground_segmentation.git
+cd linefit_ground_segmentation
+git checkout 0342d90 
+cd ..
+
+git clone git@github.com:MURDriverless/mur_common.git
+cd mur_common
+git checkout 7faf0f9
+cd ..
+
+git clone git@github.com:MURDriverless/mursim.git
+cd mursim
+git checkout d57bbe4
+cd ..
+
+git clone git@github.com:MURDriverless/mursim_actuator.git
+cd mursim_actuator
+git checkout 121d312
+cd ..
+
+git clone https://github.com/MURDriverless/ouster_example
+cd ouster_example
+git checkout d9da07e 
+cd ..
+
+git clone git@github.com:MURDriverless/path_follower_ros.git
+cd path_follower_ros
+git checkout 5cf5830
+cd ..
+
+git clone git@github.com:MURDriverless/planner_exploratory.git
+cd planner_exploratory
+git checkout 60746ed
+cd ..
+
+git clone git@github.com:MURDriverless/slam.git
+cd slam
+git checkout 53e2ce8
+cd ..
+
+git clone git@github.com:MURDriverless/localisation.git
+cd localisation
+git checkout cb9a8da 
+cd ..
+
+git clone git@github.com:MURDriverless/vision_opencv.git
+cd vision_opencv
+git checkout 1bab9c8 
+cd ..
